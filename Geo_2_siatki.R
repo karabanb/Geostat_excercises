9302de59b09978f186f5ff51eb01bfc247af1e69
karabanb <b.karaban@me.com>
2018-04-06 18:31:29 +0200

    nowe biblioteki i budowa siatki rastrowej klasy RasterLayer

--- Geo_2_siatki.R
+++ Geo_2_siatki.R
@@ -4,6 +4,8 @@ library(ggplot2)
 library(corrplot)
 library(geostatbook)
 library(rgdal)
+library(raster)
+library(rasterVis)
 
 data(punkty)
 data(granica)
@@ -11,13 +13,11 @@ data(granica)
 
 ####### tworzenie siatki #########
 
-## okreslenie granic dla punktow
-
-bbox(punkty)
+bbox(punkty) # okreslenie granic dla punktow
 
 ## tworzenie siatki
-siatka <- expand.grid(x = seq(min(siatka$x), max(siatka$x), length.out = 100),
-                      y = seq(min(siatka$y), max(siatka$y), length.out = 100))
+siatka <- expand.grid(x = seq(min(punkty$x), max(punkty$x), length.out = 10),
+                      y = seq(min(punkty$y), max(punkty$y), length.out = 10))
 
 
 coordinates(siatka) <- ~x + y  ## nadanie wspolrzednych
@@ -32,22 +32,33 @@ coordinates(siatka2) <- ~x + y
 gridded(siatka2) <- TRUE
 proj4string(siatka2) <- proj4string(punkty)
 
-### siatki regularne 
+### siatki regularne ####
 
 plot(siatka)
 points(punkty, pch = 1)
 
 plot(siatka2)
-points(punkty, pch = 2)
+points(punkty, pch = ".")
 
-### siatki nieregularne rastrowe
+### siatki nieregularne - klasa RasterLayer ####
 
 
-granica <- readOGR(".", layer = "granica") # przyklad zaczytania pliku shp, prj, bdf, czy shx
+# granica <- readOGR(".", layer = "granica") # przyklad zaczytania pliku shp, prj, bdf, czy shx
 
 plot(granica)
 
-siatka_n <- 
+siatka_n <- raster(extent(granica))
+res(siatka_n) <- c(250, 250)    # okreslanie rozdzielczosci siatki
+siatka_n[] <- 0  # ustalenie wszystkich oczek siatki na 0
+proj4string(siatka_n) <- proj4string(granica)  #ujednolicenie ukladu wspolrzednych
+siatka_n <- mask(siatka_n, granica) # przyciecie siatki do granicy
 
+levelplot(siatka_n, margin = FALSE)
 
+## siatki nieregularne - klasa SpatialPixelsDataFrame ####
+
+siatka_n <- as(siatka_n, "SpatialPointsDataFrame") # przetworzenie do odpowiedniej klasy
+siatka_n <- siatka_n[!is.na(siatka_n@data$layer), ]
+gridded(siatka_n) <- TRUE
+plot(siatka_n)
 
