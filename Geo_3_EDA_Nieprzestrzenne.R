818bd317cfeaec3f5fa73cedf34f6d68fda6a685
karabanb <b.karaban@me.com>
2018-04-08 13:44:22 +0200

    Dodanie EDA i wykresow

--- Geo_3_EDA_Nieprzestrzenne.R
+++ Geo_3_EDA_Nieprzestrzenne.R
@@ -11,4 +11,32 @@ library(geostatbook)
 data(punkty)
 data(granica)
 
+### EDA ###
+
+str(punkty) # sprawdzenie zawartości obiektu klasy `SpatialPointsDataFrame`
+str(punkty@data) #Sprawdzenie tabeli atrybutów dla zbioru punktów
+
+punkty$temp # dostep do wybranej kolumny w 1 sposob
+punkty@data$temp # dostep do wybranej kolumny w 2 sposob
+
+plot(punkty)
+plot(granica, add = TRUE) # sprawdzenie czy punkty znajduja sie w obrebie granicy
+
+### Statystki opisowe ###
+
+summary(punkty@data) #kwantyle
+sd(punkty$temp) #odchylenie standardowe
+
+ggplot(punkty@data, aes(temp)) + geom_histogram() #histogram
+
+ggplot(punkty@data, aes(temp)) + geom_density() # rozklad gestosci
+plot(density(punkty$temp)) #tez rozklad gestosci ale z base graphics
+
+ggplot(punkty@data, aes(sample = temp)) + stat_qq() # wykres qqplot
+qqnorm(punkty$temp) #alternatywnie z base graphics
+
+
+
+
+
 
