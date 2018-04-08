d11c655aec26729fadeebe297cf8946c6570ca9d
karabanb <b.karaban@me.com>
2018-04-08 14:24:52 +0200

    korelacje, dystrybuanta, ANOVA

--- Geo_3_EDA_Nieprzestrzenne.R
+++ Geo_3_EDA_Nieprzestrzenne.R
@@ -22,7 +22,7 @@ punkty@data$temp # dostep do wybranej kolumny w 2 sposob
 plot(punkty)
 plot(granica, add = TRUE) # sprawdzenie czy punkty znajduja sie w obrebie granicy
 
-### Statystki opisowe ###
+### Statystki opisowe, podstawowe wykresy, korelacje ###
 
 summary(punkty@data) #kwantyle
 sd(punkty$temp) #odchylenie standardowe
@@ -35,8 +35,25 @@ plot(density(punkty$temp)) #tez rozklad gestosci ale z base graphics
 ggplot(punkty@data, aes(sample = temp)) + stat_qq() # wykres qqplot
 qqnorm(punkty$temp) #alternatywnie z base graphics
 
+ggplot(punkty@data, aes(temp)) + stat_ecdf() # dystrubuanta empiryczna
+graphics::plot(ecdf(punkty$temp),pch = ".") #alternatywnie z base graphics
 
+pairs.default(punkty@data, pch =".") # wykresy rozrzutu
 
+cor_matrix <- cor(punkty@data) #macierz korelacji
+cor_matrix
+corrplot(cor_matrix)
 
+cor.test(punkty$ndvi, punkty$savi) # testowanie istotnosci korelacji
+
+punkty$clc <- as.factor(punkty$clc)
+ggplot(punkty@data, aes(clc, temp)) + geom_boxplot() #klasyczny boxplot
+
+punkty$clc <- as.factor(punkty$clc) # ANOVA sprawdzenie istotnosci roznic miedzy srednimi
+aov_test <- aov(temp~clc, data = punkty)
+summary(aov_test)
+
+tukey <- TukeyHSD(aov_test, "clc")
+plot(tukey) #wizualizacja ronznic z przedzialami ufnosci
 
 
