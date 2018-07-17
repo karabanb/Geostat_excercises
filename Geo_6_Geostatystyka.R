675ec34db0bccbeb2bf20a9ca63a67687aec4c2f
Bartłomiej Karaban <bkaraban@kruk-inkaso.com.pl>
2018-07-17 11:08:45 +0200

    wskazanie bibliotek i ramek danych

---
+++ Geo_6_Geostatystyka.R
@@ -0,0 +1,20 @@
+
+#### biblioteki ################################################################################
+
+library("gstat")
+library("sp")
+library("pgirmess")
+library("ggplot2")
+library("raster")
+library("rasterVis")
+library("geostatbook")
+
+#### data ######################################################################################
+
+data(punkty)
+data(siatka)
+data(granica)
+load("data/some_district.Rdata")
+
+
+####
